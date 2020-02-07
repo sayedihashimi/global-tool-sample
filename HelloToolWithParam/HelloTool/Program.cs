d4056a78fd1b7c5eaf9c581c36ae9d43f0b19946
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-02-07 11:40:51 -0500

    adding parameters

--- HelloToolWithParam/HelloTool/Program.cs
+++ HelloToolWithParam/HelloTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using McMaster.Extensions.CommandLineUtils;
 
 namespace HelloTool {
@@ -6,8 +7,21 @@ namespace HelloTool {
         static int Main(string[] args) {
             var app = new CommandLineApplication();
 
+            app.HelpOption();
+            app.UsePagerForHelpText = false;
+            var optionName = app.Option<string>("-n|--name <NAME>", "Your name (required)", CommandOptionType.SingleValue);
+            optionName.IsRequired(allowEmptyStrings: false);
+            var optionAge = app.Option<int>("-a|--age <AGE>", "Your age", CommandOptionType.SingleValue);
+
             app.OnExecute(() => {
-                Console.WriteLine("Hello World, using the Builder API!");
+                var name = optionName.Value();
+                string message = $"Hello {name}";
+
+                if (optionAge.HasValue()) {
+                    message += $", who is {optionAge.Value()} years old";
+                }
+
+                Console.WriteLine(message);
             });
 
             return app.Execute(args);
