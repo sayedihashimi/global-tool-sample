a362bcee300c52e4f0ac33593b96feb8405b6e20
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-02-07 09:44:42 -0500

    adding sample using CommandLineUtils

---
+++ HelloToolWithParam/HelloTool/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace HelloTool {
+    class Program {
+        static int Main(string[] args) {
+            var app = new CommandLineApplication();
+
+            app.OnExecute(() => {
+                Console.WriteLine("Hello2 World!");
+            });
+
+            return app.Execute(args);
+        }
+    }
+}
