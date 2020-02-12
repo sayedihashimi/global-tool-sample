e81ba9826b42add14a6d10dc171c93dfc27223d9
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-02-12 12:05:23 -0500

    clean up

--- HelloToolWithParam/HelloTool/Program.cs
+++ HelloToolWithParam/HelloTool/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Microsoft.VisualBasic;
 
 namespace HelloTool {
     class Program {
@@ -8,8 +7,8 @@ namespace HelloTool {
         /// </summary>
         /// <param name="name">Your name (required)</param>
         /// <param name="age">Your age</param>
-        static void Main(string name = "unknown", int age = 10) {
-            string message = age <= 0 ? $"Hello2 {name}" : $"Hello {name}, who is {age} years old";
+        static void Main(string name = "World", int age = 0) {
+            string message = age <= 0 ? $"Hello there {name}!" : $"Hello there {name}, who is {age} years old";
 
             Console.WriteLine(message);
         }
